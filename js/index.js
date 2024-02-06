0b8cb2e4a7830615325f68013e7e2b86ad64c77f
Ona-kri4ala-xvatit <live4ever4@gmail.com>
2024-02-06 17:00:07 +0400

    Edit code

--- js/index.js
+++ js/index.js
@@ -3,6 +3,8 @@ loaderTxt.classList.add('loading-text');
 loaderTxt.textContent = 'Идет загрузка...';
 document.body.append(loaderTxt);
 
+let postFlag = false;
+
 fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json()).then(users => {
     loaderTxt.textContent = "";
     const usersWrapper = document.createElement('div');
@@ -12,25 +14,38 @@ fetch('https://jsonplaceholder.typicode.com/users').then(response => response.js
         const userItems = document.createElement('div');
         userItems.classList.add('user-items');
 
-        const userId = document.createElement('div');
-        userId.classList.add('user-item');
-        userId.textContent = 'ID: ' + user.id;
-        userItems.append(userId);
+        createUserItemBlock(userItems, user);
 
-        const userName = document.createElement('div');
-        userName.classList.add('user-item');
-        userName.textContent = 'Name: ' + user.name;
-        userItems.append(userName);
+        userItems.addEventListener('click', e => {
+            if (!postFlag) {
+                fetch(`https://jsonplaceholder.typicode.com/posts?userId=${user.id}`)
+                    .then(response => response.json())
+                    .then(posts => {
+                        posts.forEach(post => {
+                            postFlag = true;
+                            const userPost = document.createElement('div');
+                            userPost.classList.add('user-post-items');
 
-        const userEmail = document.createElement('div');
-        userEmail.classList.add('user-item');
-        userEmail.textContent = 'E-mail: ' + user.email;
-        userItems.append(userEmail);
+                            const postId = document.createElement('div');
+                            postId.classList.add('user-post-item');
+                            postId.textContent = 'User Id: ' + post.userId + ' | ' + 'ID: ' + post.id;
+                            userPost.append(postId);
 
-        const userAddress = document.createElement('div');
-        const addressJson = JSON.stringify(user.address);
-        userAddress.textContent = `Address: ${addressJson}`
-        userItems.append(userAddress);
+                            const Title = document.createElement('div');
+                            Title.classList.add('user-post-item');
+                            Title.textContent = 'Title:' + post.title;
+                            userPost.append(Title);
+
+                            const Body = document.createElement('div');
+                            Body.classList.add('user-post-item');
+                            Body.textContent = 'Body:' + post.body;
+                            userPost.append(Body);
+
+                            document.body.append(userPost);
+                        });
+                    });
+            }
+        });
 
         usersWrapper.append(userItems);
     });
@@ -38,4 +53,24 @@ fetch('https://jsonplaceholder.typicode.com/users').then(response => response.js
     document.body.append(usersWrapper);
 });
 
+function createUserItemBlock(userItems, user) {
+    const userId = document.createElement('div');
+    userId.classList.add('user-item');
+    userId.textContent = 'ID: ' + user.id;
+    userItems.append(userId);
 
+    const userName = document.createElement('div');
+    userName.classList.add('user-item');
+    userName.textContent = 'Name: ' + user.name;
+    userItems.append(userName);
+
+    const userEmail = document.createElement('div');
+    userEmail.classList.add('user-item');
+    userEmail.textContent = 'E-mail: ' + user.email;
+    userItems.append(userEmail);
+
+    const userAddress = document.createElement('div');
+    const addressJson = JSON.stringify(user.address);
+    userAddress.textContent = `Address: ${addressJson}`
+    userItems.append(userAddress);
+}
